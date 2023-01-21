7bc67316f60a7a5a7957f928ca82e9c7c6ebf26e
floppa1511 <107504228+floppa1511@users.noreply.github.com>
2023-01-21 16:53:13 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
-<?php
-echo "In Development.\nDiscord: http://invite.xenoth.top";
-echo 'Your current IP(Not Logged):'.$_SERVER['REMOTE_ADDR'];
-?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Xenoth</title>
+</head>
+<body>
+<h2>In Development.\nDiscord: http://invite.xenoth.top</h2>
+<h1><?php echo 'Your current IP(Not Logged):'.$_SERVER['REMOTE_ADDR']; ?></h1>
+</body>
+</html>
